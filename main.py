17fa7dfc576a0dec41200d257eabccaa3b43ad5e
Roy Kakkenberg <me@roykakkenberg.nl>
2023-11-21 13:52:57 +0100

    Change audio player to VLC

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import os
-from io import BytesIO
 import socketio
 from dotenv import load_dotenv
-from pydub import AudioSegment
-from pydub.playback import _play_with_simpleaudio
-from simpleaudio.shiny import PlayObject
+import vlc
 import time
 import requests
 import logging
@@ -17,13 +14,11 @@ logging.basicConfig(level=LOG_LEVEL)
 namespace = '/audio'
 running = True
 sio = socketio.Client(logger=True)
-audio_original: AudioSegment | None = None
-audio_timestamped: AudioSegment | None = None
-playback: PlayObject | None = None
+player: vlc.MediaPlayer | None = None
 
 
 def main():
-    global sio, audio_original, playback, running
+    global sio, player, running
 
     url = os.environ['URL'] + '/api/auth/mock'
     result = requests.post(url)
@@ -33,66 +28,74 @@ def main():
     sio.connect(os.environ['URL'], headers={'cookie_development': 'connect.sid=' + cookie},
                 namespaces=[namespace])
 
-    print('Connected')
+    logging.info('Connected')
 
     try:
         while running:
             time.sleep(0.5)
     except KeyboardInterrupt:
         running = False
-        if playback is not None and playback.is_playing():
-            playback.stop()
+        if player is not None and player.is_playing():
+            player.stop()
 
 
 @sio.event(namespace=namespace)
-def play_audio():
-    global playback, audio_original, audio_timestamped
-    print('receive play event')
+def play_audio(seconds=0):
+    global player
+    logging.info('receive play event')
 
-    if audio_timestamped is not None:
-        playback = _play_with_simpleaudio(audio_timestamped)
-        audio_timestamped = None
-    else:
-        playback = _play_with_simpleaudio(audio_original)
+    if player is None:
+        return
+
+    if player.play() < 0:
+        raise Exception('Could not start playback')
+
+    player.set_time(seconds * 1000)
 
     sio.emit('play_audio_started', int(time.time() * 1000), namespace=namespace)
 
 
 @sio.event(namespace=namespace)
 def stop_audio():
-    global playback
+    global player
 
-    print('receive stop event')
-    if playback is not None:
-        playback.stop()
+    logging.info('receive stop event')
+    if player is not None:
+        player.stop()
 
 
 @sio.event(namespace=namespace)
 def skip_to(seconds):
-    global audio_original, playback, audio_timestamped
+    global player
 
-    audio_timestamped = audio_original[seconds * 1000:]
-    if playback and playback.is_playing():
-        playback.stop()
-        playback = _play_with_simpleaudio(audio_timestamped)
-        audio_timestamped = None
+    logging.info('receive skip event: ' + str(seconds))
+
+    if player is None:
+        return
+
+    position = seconds * 1000
+    player.set_time(position)
 
 
 @sio.event(namespace=namespace)
 def load_audio(url: str):
-    global playback, audio_original
-    logging.info('load audio: ' + url)
+    global player
+    full_url = os.environ['URL'] + url
+    logging.info('load audio: ' + full_url)
     stop_audio()
 
     try:
-        # Fetch file from source
-        res = requests.get(os.environ['URL'] + url)
-        if res.status_code >= 300:
-            raise Exception('Status code ' + str(res.status_code), res.content)
+        # creating a vlc instance
+        vlc_instance: vlc.Instance = vlc.Instance()
 
-        file_name, file_extension = os.path.splitext(url)
-        logging.info('Fetched audio file. Now initializing...')
-        audio_original = AudioSegment.from_file(BytesIO(res.content), file_extension[1:])
+        # creating a media player
+        player = vlc_instance.media_player_new()
+
+        # creating a media
+        media: vlc.Media = vlc_instance.media_new(full_url)
+
+        # setting media to the player
+        player.set_media(media)
 
         sio.emit('load_audio_success', namespace=namespace)
         logging.info('Audio file initialized!')
